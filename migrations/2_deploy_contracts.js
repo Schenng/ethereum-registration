28a1ddb2608487dc3be335ea630aaea55dd238c6
Schenng <scheng365@gmail.com>
2017-09-04 01:39:49 -0400

    Remove truffle default files

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,11 +1,5 @@
-// var ConvertLib = artifacts.require("./ConvertLib.sol");
-// var MetaCoin = artifacts.require("./MetaCoin.sol");
 var Registration = artifacts.require("./Registration.sol");
 
-
 module.exports = function(deployer) {
-  // deployer.deploy(ConvertLib);
-  // deployer.link(ConvertLib, MetaCoin);
-  // deployer.deploy(MetaCoin);
   deployer.deploy(Registration)
 };
